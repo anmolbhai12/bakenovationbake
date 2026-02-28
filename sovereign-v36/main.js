8b5796b2964a9b91b999d2e3d7862e9400b66f00
anmolbhai12 <anmolji04013.com@gmail.com>
2026-02-28 12:20:01 +0530

    fix: update GAS URL to final working deployment (Anyone access)

--- sovereign-v36/main.js
+++ sovereign-v36/main.js
@@ -131,7 +131,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const userNavArea = document.getElementById('user-nav-area');
 
     // Unified Google Apps Script URL
-    const UNIFIED_GAS_URL = 'https://script.google.com/macros/s/AKfycbxIBZDy9YAWsw2TEcmPytUIxEwaMtfxIrmdbNq6ABPo4yqaxiO_1ni5frvriGO1ey90/exec';
+    const UNIFIED_GAS_URL = 'https://script.google.com/macros/s/AKfycbxyRviChlnkUCq-M8FsglbT0d4pbzrR8-G7oYac4GEPiTrBkkusbkXWwDmc0qx1OUfcbQ/exec';
 
     const EMAIL_PROXY_URL = UNIFIED_GAS_URL;
     const EMAIL_SIGNUP_SHEET_URL = UNIFIED_GAS_URL;
